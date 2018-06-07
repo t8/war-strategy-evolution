50db70389fd4721dfbde2dc158fbf9ef1b473f07
tate <tateflyer1@gmail.com>
2018-06-07 19:48:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
+// STATIC VARIABLES
 var timeline = document.getElementById('timeline-pos');
+var radio = document.getElementById('radio');
+var gas = document.getElementById('mustard');
+var engine = document.getElementById('engine');
+var radar = document.getElementById('radar');
+var nuke = document.getElementById('nuke');
 
+var radioCard = document.getElementById('radioCard');
+var gasCard = document.getElementById('mustardCard');
+var engineCard = document.getElementById('engineCard');
+var radarCard = document.getElementById('radarCard');
+var nukeCard = document.getElementById('nukeCard');
+
+// DYNAMIC VARIABLES
+var currentCard = timeline;
+
+// SCROLL EVENTS
 $(window).scroll(function() {
-    if ( $(window).scrollTop() > $(timeline).offset().top ) {
-        $('a.sf-back-to-top').fadeIn('slow');
-    } else {
-        $('a.sf-back-to-top').fadeOut('slow');
-    }
+	if ($(window).scrollTop() > $(timeline).offset().top) {
+		$('a.sf-back-to-top').fadeIn('slow');
+	} else {
+		$('a.sf-back-to-top').fadeOut('slow');
+	}
+	if ($(window).scrollTop() > $(radio).offset().top) {
+		currentCard = radioCard;
+		// console.log("here 1");
+		if ($(window).scrollTop() > $(gas).offset().top) {
+			currentCard = gasCard;
+			// console.log("here 2");
+			if ($(window).scrollTop() > $(engine).offset().top) {
+				currentCard = engineCard;
+				// console.log("here 3");
+				if ($(window).scrollTop() > $(radar).offset().top) {
+					currentCard = radarCard;
+					// console.log("here 4");
+					if ($(window).scrollTop() > $(nuke).offset().top) {
+						currentCard = nukeCard;
+						// console.log("here 5");
+					}
+				}
+			}
+		}
+	}
 });
 
 $('a.sf-back-to-top').click(function() {
-    $('html, body').animate({
-        scrollTop: $(timeline).offset().top
-    }, 700);
-    return false;
+	$('html, body').animate(
+		{
+			scrollTop: $(currentCard).offset().top,
+		},
+		700
+	);
+	return false;
 });
